e15ec2b879c1c826f61f0b563980f915e0f1c3b4
Craig Hesling <craig@hesling.com>
2017-09-06 02:14:09 -0400

    Added simple starter service

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/openchirp/framework"
+)
+
+const (
+	defaultFrameworkURI = "http://localhost:7000"
+	defaultBrokerURI    = "tls://localhost:1883"
+	defaultServiceID    = ""
+	defaultServiceToken = ""
+)
+
+/* Options to be filled in by arguments */
+var frameworkURI string
+var brokerURI string
+var serviceID string
+var serviceToken string
+
+/* HACK to allow us to use raw MQTT during conversion period */
+var mqttQos int64
+
+/* Setup argument flags and help prompt */
+func init() {
+	flag.StringVar(&frameworkURI, "framework", defaultFrameworkURI, "Sets the HTTP REST framework server URI")
+	flag.StringVar(&brokerURI, "broker", defaultBrokerURI, "Sets the MQTT broker URI associated with the framework server")
+	flag.StringVar(&serviceID, "id", defaultServiceID, "Sets the service ID associated with this service instance")
+	flag.StringVar(&serviceToken, "token", defaultServiceToken, "Sets the service access token associated with this instance")
+}
+
+func main() {
+	// var lorawanMQTTQos string
+	// var lorawanMQTTBroker string
+	// var lorawanMQTTUser string
+	// var lorawanMQTTPass string
+
+	flag.Parse()
+
+	c, err := framework.StartServiceClient(frameworkURI, brokerURI, serviceID, serviceToken)
+	if err != nil {
+		fmt.Println("Error - ", err)
+	}
+
+	err = c.SetStatus("Started GW service")
+	if err != nil {
+		fmt.Println("Error - ", err)
+	}
+
+	updates, err := c.StartDeviceUpdates()
+	if err != nil {
+		fmt.Println("Error - ", err)
+	}
+
+	fmt.Println("Success!!!!")
+
+	for update := range updates {
+		fmt.Println(update)
+		c.SetDeviceStatus(update.Id, "Asked to reg gwid \"", update.Config["Gateway ID"], "\" at ", time.Now().Format(time.UnixDate))
+	}
+
+	c.StopClient()
+}
